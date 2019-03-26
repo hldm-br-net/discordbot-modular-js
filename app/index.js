15668c9f88bfc29ebaa3d72254e7f3f2de2db166
Rafael Alves <rafa.max81@gmail.com>
2019-03-26 20:05:23 -0300

    Finally, after suffering 3 major rewrites (and being so lazy), here is the new version!
    - Rewritten from scratch
    - Modular and flexible
    - Multi-language support

---
+++ app/index.js
@@ -0,0 +1,247 @@
+#!/usr/bin/env node
+/**
+ * Guts - A Discord bot made for HLDM-BR.NET.
+ * 
+ * MIT License
+ * 
+ * Copyright (c) 2019 Rafael "R4to0" Alves
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+"use strict";
+
+// Static module imports ESM (disabled due to some limitations with cache system)
+//import Discord from 'discord.js';
+//import ('expose-gc');
+//import fs from 'fs';
+//import multilang from 'multi-lang';
+//import path from 'path';
+//import readline from 'readline';
+
+// Static module imports
+const fs = require('fs');
+const gc = require('expose-gc/function');
+const Discord = require('discord.js');
+const multilang = require('multi-lang');
+const path = require('path');
+const readline = require('readline');
+
+// Utils
+const utils = require('./utils/utils.js');
+
+const g_basedir = path.resolve(__dirname);
+
+// Yeah feel free to blame me on about writing this way...
+class GutsBot {
+    constructor() {
+        this.m_moduledir = `${g_basedir}/modules`;
+
+        this.m_config = require(`./settings/bot.json`);
+
+        this.m_lang = this.m_config.lang;
+
+        this.m_bot = new Discord.Client({ autoReconnect: true });
+        this.m_bot.collection = new Discord.Collection();
+        this.m_cooldowns = new Discord.Collection();
+        this.m_multilang = multilang(`./app/lang/lang.json`, this.m_lang, false); // hardcoded path
+    }
+
+    async Run() {
+        // Listen for errors
+        this.m_bot.on("error", console.error);
+        this.m_bot.on("warn", console.warn);
+
+        await this.Loader();
+
+        this.m_bot.login(this.m_config.token);
+
+        this.ListenMsgs();
+    }
+
+    // Load modules
+    async Loader() {
+        let count = 0;
+        let failed = 0;
+        let files = fs.readdirSync(this.m_moduledir).filter(file => file.endsWith('.js'));
+
+        // Iterate though all files
+        for await (let file of files) {
+            let name = path.parse(file).name;
+
+            // File already registered, skip it!
+            if (this.m_bot.collection.has(name)) {
+                utils.printmsg(`${this.m_multilang('ML_MODULES_ALREADYREGISTERED', { file: file })}`);
+                //failed++
+                continue;
+            }
+
+            try {
+                // Load module, instance a class and call it.
+                let loadfile = require(`${this.m_moduledir}/${file}`);
+                let object = new loadfile(this); // expose this class
+                object.Init(); // call setup
+
+                // Register module in the map collector
+                this.m_bot.collection.set(name, object);
+
+                utils.printmsg(`${this.m_multilang('ML_MODULE_FILELOADED', { file: file })}`, 2);
+
+                // Destroy references, just in case
+                loadfile = null;
+                object = null;
+
+                count++;
+            }
+            catch (error) {
+                utils.printmsg(`${this.m_multilang('ML_MODULE_FILEFAIL', { file: file })}`, 2);
+                utils.printmsg(error, 3);
+                failed++;
+            }
+        }
+
+        if (count) utils.printmsg(this.m_multilang('ML_MODULES_LOADED', { cnt: count }));
+        if (failed) utils.printmsg(this.m_multilang(this.m_config.verbose >= 3 ? 'ML_MODULES_FAILED_VERB' : 'ML_MODULES_FAILED', { fcnt: failed }));
+        return;
+    }
+
+    // Listen for messages
+    ListenMsgs() {
+        this.m_bot.once('ready', () => {
+            utils.printmsg(`${this.m_multilang('ML_LOGINMSG')} ${this.m_bot.user.username}!`);
+            this.m_bot.user.setActivity(`the game`);
+        });
+
+        this.m_bot.on('message', message => {
+            // If the message has no prefix, or if the message comes from a bot, don't do anything
+            if (!message.content.startsWith(this.m_config.prefix) || message.author.bot) return;
+
+            //Removes prefix, split commands and arguments
+            const args = message.content.slice(this.m_config.prefix.length).split(/ +/g);
+
+            // Get command name only
+            const commandName = args.shift().toLowerCase();
+
+            // Commands/alias handler.
+            const command = this.m_bot.collection.get(commandName) || this.m_bot.collection.find(cmd => cmd.m_aliases && cmd.m_aliases.includes(commandName));
+
+            // Unknown command, do nothing
+            if (!command) return;
+
+            // Checks if we can use that command over PMs
+            if (command.m_guildonly && message.channel.type !== 'text') return message.reply(this.m_multilang('ML_PM_NOTAVAILABLE'));
+
+            // If there is no args, show how to use such command
+            if (command.m_args && !args.length) {
+                if (command.m_usage) {
+                    return message.channel.send(`${message.author} ${this.m_multilang('ML_COMMAND_USAGE')}: \`${this.m_config.prefix}${command.m_command} ${command.m_usage}\``);
+                }
+            }
+
+            // Cooldown feature
+            if (!this.m_cooldowns.has(command.m_command)) {
+                this.m_cooldowns.set(command.m_command, new Discord.Collection());
+            }
+
+            const now = Date.now();
+            const timestamps = this.m_cooldowns.get(command.m_command);
+            const cooldownAmount = (command.m_cooldown || 3) * 1000;
+
+            if (timestamps.has(message.author.id)) {
+                const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+                if (now < expirationTime) {
+                    const timeLeft = (expirationTime - now) / 1000;
+                    return message.reply(`${this.m_multilang('ML_COMMAND_COOLDOWN', { time: timeLeft.toFixed(1), command: command.m_command })}`);
+                }
+            }
+
+            timestamps.set(message.author.id, now);
+            setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+            // Try to run that command
+            try {
+                command.execute(message, args);
+            }
+            catch (error) {
+                message.reply(this.m_multilang('ML_INTERNAL_ERROR_COMMAND'));
+                utils.printmsg(`Failed to execute command ${message}`, 2);
+                utils.printmsg(error, 3);
+            }
+        });
+
+    }
+
+    async Destroy() {
+        return this.m_bot.destroy();
+    }
+
+};
+
+let BotInstance = new GutsBot();
+
+// Send unhandled promise error messages over PM
+process.on("unhandledRejection", err => {
+    console.error(`Unhandled promise rejection!\n${err.stack}`);
+    try {
+        BotInstance.m_bot.users.get("85509507316219904").send(err.stack); // SEND IT TO ME! R4to0#2874
+    }
+    catch (error) {
+        process.exit();
+    }
+});
+
+
+// Catch CTRL+C on Windows. Requires 'readline'.
+// https://stackoverflow.com/questions/10021373/what-is-the-windows-equivalent-of-process-onsigint-in-node-js
+if (process.platform === "win32") {
+    let rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.on("SIGINT", function () {
+        process.emit("SIGINT");
+    });
+}
+
+// Quit on CTRL+C, destroy bot session
+process.on("SIGINT", function () {
+    console.log("CTRL + C DETECTED, GRACEFULLY CLOSING...");
+    BotInstance.Destroy().then(process.exit());
+});
+
+// Custom event to restart without killing process
+process.on("RESTART", function () {
+    console.log("Killing main class instance...");
+    try {
+        (async () => {
+            BotInstance.Destroy().then(BotInstance = null).then(gc());
+            console.log("Spawning new class instance...");
+            BotInstance = new GutsBot();
+            BotInstance.Run(); // henlo
+        })();
+    }
+    catch (error) {
+        console.log(error);
+        process.exit();
+    }
+});
+
+BotInstance.Run(); // henlo
---
+++ app/utils/utils.js
@@ -0,0 +1,154 @@
+/**
+ * Guts - A Discord bot made for HLDM-BR.NET.
+ * 
+ * MIT License
+ * 
+ * Copyright (c) 2019 Rafael "R4to0" Alves
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+"use strict";
+
+const config = require(`../settings/bot.json`);
+
+/**
+* Return years, months, days, hours, mins and secs from int value.
+* Limited by its max range:
+*
+* Seconds: 0-59
+* Minutes: 0-59
+* Hours: 0-23
+* Days: 1-30
+* Months: 1-12
+*
+* Does not display if result is 0.
+*
+* Formula base
+* hour in seconds: 60*60 = 3600
+* day in seconds: 60*60*24 = 86400
+* month in seconds: 60*60*24*30 = 2592000
+* year in seconds: 60*60*24*30*12 = 31104000
+*
+* ... but a year is 31536000 compared to previous calculation (diff 432000 = 5 days)
+*
+* @param {number} value Time in seconds.
+* @returns {string} Time in format 00A 00M 00d 00h 00m where each value is > 0.
+*/
+function FormatTime(seconds) {
+    const sec = Math.floor(seconds % 60);
+    const min = Math.floor((seconds % 3600) / 60);
+    const hrs = Math.floor((seconds % 86400) / 3600);
+    const dys = Math.floor((seconds % 2592000) / 86400);
+    const mts = Math.floor((seconds % 31104000) / 2592000);
+    const yrs = Math.floor((seconds / 31536000));
+
+    const yShow = yrs > 0 ? yrs + (yrs == 1 ? "A " : "A ") : "";
+    const MShow = mts > 0 ? mts + (mts == 1 ? "M " : "M ") : "";
+    const dShow = dys > 0 ? dys + (dys == 1 ? "D " : "D ") : "";
+    const hShow = hrs > 0 ? hrs + (hrs == 1 ? "h " : "h ") : "";
+    const mShow = min > 0 ? min + (min == 1 ? "m" : "m") : "";
+
+    return yShow + MShow + dShow + hShow + mShow;
+}
+
+/**
+ * Return current local date in ISO format
+ * 
+ * @returns {string} Current date in YYYY-MM-DD HH:MM:SS
+ */
+function GetLocalDate() {
+    const date = new Date();
+    var ten = function (i) {
+        return (i < 10 ? '0' : '') + i;
+    },
+        YYYY = date.getFullYear(),
+        MM = ten(date.getMonth() + 1),
+        DD = ten(date.getDate()),
+        HH = ten(date.getHours()),
+        II = ten(date.getMinutes()),
+        SS = ten(date.getSeconds());
+    return YYYY + '-' + MM + '-' + DD + ' ' + HH + ':' + II + ':' + SS;
+}
+
+/**
+ * Returns time in milliseconds
+ * 
+ * @returns {number} Time in milliseconds.
+ */
+function GetTimeMS() {
+    return (new Date()).getTime();
+}
+
+/**
+ * Returns Unix timestamp.
+ * 
+ * @returns {number} Unix time.
+ */
+function GetUnixTime() {
+    return Math.round((new Date()).getTime() / 1000);
+}
+
+/**
+ * Prints debug message in the console for basic debugging
+ * 0 = silent
+ * 1 = default
+ * 2 = info
+ * >=3 = debug
+ * 
+ * @param {string} printmsg Message to print
+ * @param {number} level Message level
+ */
+function printmsg(msg, level = 1) {
+    // If 0 do nothing (silent)
+    if (!config.verbose) return;
+
+    // If level is not specified, threat as level 1.
+    if (!level || level === 1 && config.verbose >= 1)
+        console.log(`${GetLocalDate()}: ${msg}`);
+
+    // Level 2 (Info)
+    if (level === 2 && config.verbose >= 2)
+        console.log(`${GetLocalDate()}: (INFO) ${msg}`);
+
+    // Level 3 (Debug)
+    if (level >= 3 && config.verbose >= 3)
+        console.log(msg);
+}
+
+/**
+ * Simulates the sleep command from Unix
+ * 
+ * @param {number} sec 
+ */
+async function sleep(sec) {
+    return new Promise(resolve => {
+        setTimeout(resolve, sec * 1000);
+    });
+};
+
+module.exports = {
+    FormatTime,
+    GetLocalDate,
+    GetTimeMS,
+    GetUnixTime,
+    printmsg,
+    sleep
+}
+
