0677a8e95cf52f473ba42f2a783e0ef815339285
Rafael Alves <rmacielalves@outlook.com>
2019-10-15 18:01:15 -0300

    Simple ping-pong module

---
+++ app/modules/ping.js
@@ -0,0 +1,51 @@
+/**
+ * A Discord bot made for HLDM-BR.NET.
+ * 
+ * Ping/Pong module
+ * 
+ * MIT License
+ * 
+ * Copyright (c) 2019 Rafael "R4to0" Alves
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+"use strict";
+
+class ping {
+    constructor(bot) {
+        this.command = 'ping';
+        this.description = 'Ping!';
+        this.args = false;
+        this.guildonly = false;
+        this.usage = 'Pong!';
+        this.cooldown = 5;
+    }
+    Init() { }
+    Unload() { }
+
+    async execute(msg, args) {
+        const start = Date.now();
+        msg.channel.send('Pong! `Calculating...`').then(pingmsg => {
+            pingmsg.edit(`Pong! \`${Date.now() - start} ms\``);
+        });
+    }
+}
+
+module.exports = ping;
